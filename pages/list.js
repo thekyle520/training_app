46bbdb31310e31a47f93e7e592b3852658639a0e
Kyle Schrader <kyleschrader@Kyles-MacBook-Pro.local>
2022-05-26 09:11:32 -0700

    list page modifications

--- pages/list.js
+++ pages/list.js
@@ -11,8 +11,10 @@ const list = ({data}) => {
     const posts = (data[0].nextUsers[0].posts)
     const complete = (data[0].nextUsers[0].completed.map((item, i) => item.slug))
     const progress = complete.length/posts.length * 100
-    sessionStorage.setItem('posts', JSON.stringify(data[0].nextUsers[0]));
-    
+
+    useEffect(() => {
+        sessionStorage.setItem('posts', JSON.stringify(data[0].nextUsers[0]));
+    }, [])    
      
     const BorderLinearProgress = styled(LinearProgress)
     (({theme}) => ({
@@ -34,7 +36,7 @@ const list = ({data}) => {
     <div className="container mx-auto px-15 mb-8">
          <div className='flex flex-col rounded-md bg-white my-2'>
                 <div className='flex justify-center items-center rounded-lg py-2'>
-                    <h1 className='px-4'>{`${progress}%`}</h1>
+                    <h1 className='px-4'>{`${Math.round(progress)}%`}</h1>
                     <Box sx={{ width: '80%' }}>
                         <BorderLinearProgress variant="determinate" value={progress} />
                     </Box>
